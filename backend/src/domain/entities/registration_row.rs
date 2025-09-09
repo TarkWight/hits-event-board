f11d5266885f94707aaf1cc24cf48bbf351060ed
TarkWight <arsentiy.gonda@gmail.com>
2025-09-09 18:07:25 +0700

    Added registration_row entity

---
+++ backend/src/domain/entities/registration_row.rs
@@ -0,0 +1,14 @@
+use serde::{Deserialize, Serialize};
+use sqlx::FromRow;
+use time::OffsetDateTime;
+use uuid::Uuid;
+
+#[derive(Debug, Clone, FromRow, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct RegistrationRow {
+    pub event_id: Uuid,
+    pub student_id: Uuid,
+    #[serde(with = "time::serde::rfc3339")]
+    pub registered_at: OffsetDateTime,
+    pub gcal_event_id: Option<String>,
+}
