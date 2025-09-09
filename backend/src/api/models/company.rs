f7682dce289e869bbcaf90c02560a188ac16b793
TarkWight <arsentiy.gonda@gmail.com>
2025-09-09 17:04:46 +0700

    Updated company model
    -- refactoring

--- backend/src/api/models/company.rs
+++ backend/src/api/models/company.rs
@@ -1,6 +1,5 @@
 use serde::Serialize;
 use uuid::Uuid;
-use crate::domain::entities::company::Company;
 
 #[derive(Debug, Serialize)]
 pub struct CompanyOut {
@@ -8,10 +7,4 @@ pub struct CompanyOut {
     pub name: String,
     pub manager_count: Option<i64>,
     pub event_count: Option<i64>,
-}
-
-impl From<(Company, Option<i64>, Option<i64>)> for CompanyOut {
-    fn from((company, manager_count, event_count): (Company, Option<i64>, Option<i64>)) -> Self {
-        Self { id: company.id, name: company.name, manager_count, event_count }
-    }
 }
