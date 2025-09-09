04f52e090f2d79549251a72e25e5e60d6e80c627
TarkWight <arsentiy.gonda@gmail.com>
2025-09-09 17:12:26 +0700

    Refactoring company_row entity

--- backend/src/domain/entities/company_row.rs
+++ backend/src/domain/entities/company_row.rs
@@ -1,7 +1,7 @@
 use sqlx::FromRow;
 use uuid::Uuid;
+
 use crate::domain::entities::company::{Company, CompanyValidationError};
-use crate::api::requests::company::CreateCompanyIn;
 
 #[derive(Debug, Clone, FromRow)]
 pub struct CompanyRow {
@@ -19,20 +19,5 @@ impl TryFrom<CompanyRow> for Company {
 impl From<Company> for CompanyRow {
     fn from(domain: Company) -> Self {
         Self { id: domain.id, name: domain.name }
-    }
-}
-
-impl TryFrom<CreateCompanyIn> for CompanyRow {
-    type Error = String;
-
-    fn try_from(value: CreateCompanyIn) -> Result<Self, Self::Error> {
-        if value.name.trim().is_empty() {
-            return Err("Company name must not be empty".to_string());
-        }
-
-        Ok(CompanyRow {
-            id: uuid::Uuid::new_v4(),
-            name: value.name,
-        })
     }
 }
