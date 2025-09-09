3b7039ac718054e93181122c63b081c80fc260a6
TarkWight <arsentiy.gonda@gmail.com>
2025-09-09 17:07:13 +0700

    Refactoring company request

--- backend/src/api/requests/company.rs
+++ backend/src/api/requests/company.rs
@@ -1,5 +1,7 @@
 use serde::Deserialize;
-use crate::domain::entities::company::Company;
+use uuid::Uuid;
+
+use crate::domain::entities::company::{Company, CompanyPatch};
 
 #[derive(Debug, Deserialize)]
 pub struct CreateCompanyIn {
@@ -7,12 +9,18 @@ pub struct CreateCompanyIn {
 }
 
 impl From<CreateCompanyIn> for Company {
-    fn from(value: CreateCompanyIn) -> Self {
-        Company::new(uuid::Uuid::new_v4(), value.name).unwrap()
+    fn from(v: CreateCompanyIn) -> Self {
+        Company::new(Uuid::new_v4(), v.name).expect("validated")
     }
 }
 
 #[derive(Debug, Deserialize)]
 pub struct UpdateCompanyIn {
     pub name: Option<String>,
+}
+
+impl From<UpdateCompanyIn> for CompanyPatch {
+    fn from(v: UpdateCompanyIn) -> Self {
+        CompanyPatch { name: v.name }
+    }
 }
