6998cdd71843323667a9dbaf6b0c22b6fcef57ca
TarkWight <arsentiy.gonda@gmail.com>
2025-09-10 23:16:47 +0700

    Update company row entity

--- backend/src/domain/entities/company_row.rs
+++ backend/src/domain/entities/company_row.rs
@@ -1,12 +1,19 @@
 use sqlx::FromRow;
 use uuid::Uuid;
-
 use crate::domain::entities::company::{Company, CompanyValidationError};
 
 #[derive(Debug, Clone, FromRow)]
 pub struct CompanyRow {
     pub id: Uuid,
     pub name: String,
+}
+
+#[derive(sqlx::Type, Debug, Clone, Copy, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
+#[sqlx(type_name = "company_status", rename_all = "lowercase")]
+#[serde(rename_all = "lowercase")]
+pub enum CompanyStatus {
+    Active,
+    Archived,
 }
 
 impl TryFrom<CompanyRow> for Company {
