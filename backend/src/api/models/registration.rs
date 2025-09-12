e7d33bbc9f309620ae526f6053310c02f8dfd09f
TarkWight <arsentiy.gonda@gmail.com>
2025-09-12 16:42:08 +0700

    Update entity and model

--- backend/src/api/models/registration.rs
+++ backend/src/api/models/registration.rs
@@ -6,6 +6,8 @@ use uuid::Uuid;
 #[serde(rename_all = "snake_case")]
 pub struct RegistrationOut {
     pub student_id: Uuid,
+    pub student_name: String,
+    pub student_email: String,
     #[serde(with = "time::serde::rfc3339")]
     pub registered_at: OffsetDateTime,
 }
--- backend/src/domain/entities/registration_row.rs
+++ backend/src/domain/entities/registration_row.rs
@@ -8,7 +8,17 @@ use uuid::Uuid;
 pub struct RegistrationRow {
     pub event_id: Uuid,
     pub student_id: Uuid,
+    pub student_name: String,
+    pub student_email: String,
     #[serde(with = "time::serde::rfc3339")]
     pub registered_at: OffsetDateTime,
     pub gcal_event_id: Option<String>,
+}
+
+#[derive(sqlx::FromRow)]
+struct RegistrationWithUserRow {
+    student_id: Uuid,
+    registered_at: OffsetDateTime,
+    student_name: String,
+    student_email: String,
 }
